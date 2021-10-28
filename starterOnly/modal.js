d1b7649805d377f52f14aaf915af938bd67ba4b0
Camino Kevin <caminokevinn@gmail.com>
2021-10-28 04:39:53 +0200

    Modal validation + reglage pb date + finalisation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -32,17 +32,20 @@ function closeModal(){
 const chiffres = /[0-9]/; // Variable regex pour les chiffres
 const regmail = (/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/i) // Variable regex pour verifier si l'email est vaide
 const regname = (/[a-z]{2,}/i); // Variable regex pour les champs nom et prénoms
-const regbirthdate = /(19\d\d|20[0-3])(-\d\d){2}/;
+const regbirthdate = new RegExp(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/);
 const input = document.querySelectorAll("input"); // On récupère tout les input de notre formulaire dans une variable
 const radio = document.querySelectorAll("input[type=radio]"); // On récupère les input de type radio de notre formulaire dans une variable
 const checkbox = document.querySelector("input[type=checkbox]"); // On récupère les input de type checkbox de notre formulaire dans une variable
 
 document.forms[0].addEventListener("submit", (e) => {
     let erreurs = document.getElementsByClassName("erreur"); // Variable qui contiendra le message d'erreur à afficher
-    e.preventDefault;
+    const bground2 = document.querySelector('.bground2');
+    const bgroundtext = document.getElementById("bgroundtext");
+    const rectangle = document.getElementById("form");
+    e.preventDefault();
    
       if (input[0].value != input[0].value.match(regname)) { // On compare la valeur du champ prénom avec regname
-        erreurs[0].style.display = "inline";
+        erreurs[0].style.display = "block";
         input[0].style.border="2px solid red";
       }
       else {
@@ -51,7 +54,7 @@ document.forms[0].addEventListener("submit", (e) => {
       }
 
       if (input[1].value != input[1].value.match(regname)) { // On compare la valeur du champ nom avec regname
-        erreurs[1].style.display = "inline";
+        erreurs[1].style.display = "block";
         input[1].style.border="2px solid red";
       }
       else {
@@ -60,7 +63,7 @@ document.forms[0].addEventListener("submit", (e) => {
       }
 
       if (input[2].value != input[2].value.match(regmail)) { // On compare la valeur du champ prénom avec regmail
-        erreurs[2].style.display = "inline";
+        erreurs[2].style.display = "block";
         input[2].style.border="2px solid red";
       }
       else {
@@ -69,7 +72,7 @@ document.forms[0].addEventListener("submit", (e) => {
       }
 
       if (input[3].value != input[3].value.match(regbirthdate)) { // On compare la valeur du champ birthdate avec regbirthdate
-        erreurs[3].style.display = "inline";
+        erreurs[3].style.display = "block";
         input[3].style.border="2px solid red";
       }
       else {
@@ -77,8 +80,8 @@ document.forms[0].addEventListener("submit", (e) => {
         input[3].style.border="";
       }
 
-      if (input[4].value != input[4].value.match(chiffres)) { // On vérifie que le champ contient bien des nombres avec regchiffres
-        erreurs[4].style.display = "inline";
+      if (input[4].value == "") { // On vérifie que le champ birthdate est bien remplie
+        erreurs[4].style.display = "block";
         input[4].style.border="2px solid red";
       }
       else {
@@ -86,13 +89,13 @@ document.forms[0].addEventListener("submit", (e) => {
         input[4].style.border="";
       }
       if ( !radio[0].checked && !radio[1].checked && !radio[2].checked && !radio[3].checked && !radio[4].checked && !radio[5].checked) { // On check qu'une case est bien coché
-        erreurs[5].style.display = "inline";
+        erreurs[5].style.display = "block";
       }
       else {
         erreurs[5].style.display = "none";
       }
       if (!checkbox.checked) {
-        erreurs[6].style.display = "inline"; // On vérifie que la case des conditions générales est bien coché
+        erreurs[6].style.display = "block"; // On vérifie que la case des conditions générales est bien coché
       }
       else {
         erreurs[6].style.display = "none";
@@ -101,7 +104,9 @@ document.forms[0].addEventListener("submit", (e) => {
       // Si toutes les entrées sont valide , on envoi le formulaire
     if ( input[0].value && input[1].value && input[2].value && input[3].value && input[4].value &&
          (radio[0].checked || radio[1].checked || radio[2].checked || radio[3].checked || radio[4].checked || radio[5].checked) && checkbox.checked ) {
-      alert("Validation du formulaire");
+        bground2.style.display = "block";
+        bgroundtext.style.display = "block";
+        rectangle.style.display = "none";
     } else {
       e.preventDefault();
       return 0;
